2f4370f69905e2d2bb8e932bc236770c46d6be9f
Nigora Jumaeva <njumaeva@myseneca.ca>
2025-11-10 00:19:33 -0500

    Fix require paths for Vercel

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 /********************************************************************************
 * WEB322 – Assignment 02
-*
-* I declare that this assignment is my own work in accordance with Seneca's
-* Academic Integrity Policy:
-*
-* https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
-*
-* Name: Nigora Jumaeva  Student ID: 101498244 Date: November 7, 2025
-*
-* Published URL: https://vercel.com/nigorajumaevas-projects/climate-solutions/3hxyzvUjEcd6v14ZSg2cBjhCpCeE
-*
+* Name: Nigora Jumaeva   Student ID: 101498244   Date: November 7, 2025
 ********************************************************************************/
 const express = require("express");
-const projectData = require("./modules/projectData"); 
+const projectData = require("./modules/projectData");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const Sequelize = require('sequelize');
 
 let dataInitialized = false;
 projectData.initialize()
@@ -28,53 +22,45 @@ projectData.initialize()
   })
   .catch(err => console.error("Failed to initialize project data:", err));
 
-
 app.use((req, res, next) => {
-    if (!dataInitialized) {
-        return res.status(503).render("404", { message: "Server is starting, please try again shortly." });
-    }
-    next();
+  if (!dataInitialized) {
+    return res.status(503).render("404", { message: "Server is starting, please try again shortly." });
+  }
+  next();
 });
-
 
 app.get('/', (req, res) => {
-    res.render("home");
+  res.render("home");
 });
-
 
 app.get('/about', (req, res) => {
-    res.render("about");
+  res.render("about");
 });
-
 
 app.get("/solutions/projects/:id", async (req, res) => {
-    try {
-        const project = await projectData.getProjectById(parseInt(req.params.id));
-        res.render("project", { project });
-    } catch (err) {
-        res.status(404).render("404", { message: String(err) });
-    }
+  try {
+    const project = await projectData.getProjectById(parseInt(req.params.id));
+    res.render("project", { project });
+  } catch (err) {
+    res.status(404).render("404", { message: String(err) });
+  }
 });
-
 
 app.get("/solutions/projects", async (req, res) => {
-    const sector = req.query.sector;
-    try {
-        const projects = sector 
-            ? await projectData.getProjectsBySector(sector)
-            : await projectData.getAllProjects();
-        res.render("projects", { projects });
-    } catch (err) {
-        res.status(404).render("404", { message: String(err) });
-    }
+  const sector = req.query.sector;
+  try {
+    const projects = sector 
+      ? await projectData.getProjectsBySector(sector)
+      : await projectData.getAllProjects();
+    res.render("projects", { projects });
+  } catch (err) {
+    res.status(404).render("404", { message: String(err) });
+  }
 });
-
 
 app.use((req, res) => {
-    res.status(404).render("404", { message: "I am sorry, we are unable to find what you are looking for" });
+  res.status(404).render("404", { message: "I am sorry, we are unable to find what you are looking for" });
 });
 
+
 module.exports = app;
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
